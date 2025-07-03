d661950dd13d65fdce7ea58d34c0406026920a52
anandavallis <sanandavalli7@gmail.com>
2025-07-03 12:39:49 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TitleToggle from './TitleToggle';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Toggle Document Title</h1>
+      <TitleToggle />
+    </div>
+  );
+};
+
+export default App;
